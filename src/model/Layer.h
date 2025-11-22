a021a70f170f8c3606acf8a61051fc93cc611770
Vilchykau Stanislau <stanislau@goscore.me>
2025-11-22 23:31:49 +0100

    add layer template

---
+++ src/model/Layer.h
@@ -0,0 +1,17 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+class Matrix;
+class Vector;
+
+class Layer{
+public:
+    virtual Vector& forward(const Vector& input);
+    virtual Matrix& forward(const Matrix& input); 
+
+    virtual Vector& backward(const Vector& input);
+    virtual Matrix& backward(const Matrix& input); 
+};
+
+
+#endif
