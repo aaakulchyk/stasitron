8d59c350c99914a44095691c1b89d1648cb62b79
Vilchykau Stanislau <stanislau@goscore.me>
2025-11-22 23:39:08 +0100

    add serializer

--- src/model/Layer.h
+++ src/model/Layer.h
@@ -3,14 +3,17 @@
 
 class Matrix;
 class Vector;
+class Serializer;
 
 class Layer{
 public:
-    virtual Vector& forward(const Vector& input);
-    virtual Matrix& forward(const Matrix& input); 
+    virtual Vector* forward(const Vector* input);
+    virtual Matrix* forward(const Matrix* input); 
 
-    virtual Vector& backward(const Vector& input);
-    virtual Matrix& backward(const Matrix& input); 
+    virtual Vector* backward(const Vector* input);
+    virtual Matrix* backward(const Matrix* input); 
+
+    virtual Serializer& get_serializer();
 };
 
 
