470287bed5e95db44ad830f62b9e0580aefae5ee
Andrei Kulchyk <andrei.kulchik@gmail.com>
2025-11-23 01:21:58 +0100

    add `resize`, `capacity`, upd some methods to `const`

--- src/la/vector.h
+++ src/la/vector.h
@@ -22,6 +22,11 @@ public:
     ~Vector();
 
     [[nodiscard]]
+    std::size_t capacity() const;
+
+    void resize(std::size_t new_size);
+
+    [[nodiscard]]
     std::size_t size() const;
 
     // [[nodisczard]]
@@ -50,45 +55,45 @@ public:
     bool operator!=(const Vector<T>& other) const;
 
     [[nodiscard]]
-    Vector<T> operator+(const T& scalar);
+    Vector<T> operator+(const T& scalar) const;
 
     void operator+=(const T& scalar);
 
     [[nodiscard]]
-    Vector<T> operator+(const Vector<T>& other);
+    Vector<T> operator+(const Vector<T>& other) const;
 
     void operator+=(const Vector<T>& other);
 
     [[nodiscard]]
-    Vector<T> operator-(const T& scalar);
+    Vector<T> operator-(const T& scalar) const;
 
     void operator-=(const T& scalar);
 
     [[nodiscard]]
-    Vector<T> operator-();
+    Vector<T> operator-() const;
 
     [[nodiscard]]
-    Vector<T> operator-(const Vector<T>& other);
+    Vector<T> operator-(const Vector<T>& other) const;
 
     void operator-=(const Vector<T>& other);
 
     [[nodiscard]]
-    Vector<T> operator*(const T& scalar);
+    Vector<T> operator*(const T& scalar) const;
 
     void operator*=(const T& scalar);
 
     [[nodiscard]]
-    Vector<T> operator*(const Vector<T>& other);
+    Vector<T> operator*(const Vector<T>& other) const;
 
     void operator*=(const Vector<T>& other);
 
     [[nodiscard]]
-    Vector<T> operator/(const T& scalar);
+    Vector<T> operator/(const T& scalar) const;
 
     void operator/=(const T& scalar);
 
     [[nodiscard]]
-    Vector<T> operator/(const Vector<T>& other);
+    Vector<T> operator/(const Vector<T>& other) const;
 
     void operator/=(const T& other);
 
@@ -101,7 +106,30 @@ template <typename T>
 Vector<T>::Vector(std::size_t size) : data_(size) {};
 
 template <typename T>
+Vector<T>::Vector(std::size_t size, const T& v) : data_(size, v) {};
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& other) : data_(other.data_) {};
+
+template <typename T>
+Vector<T>::Vector(Vector<T>&& other) noexcept : data_(std::move(other.data_)) {};
+
+template <typename T>
+Vector<T>::Vector(std::initializer_list<T> init) : data_(init) {};
+
+template <typename T>
 Vector<T>::~Vector() = default;
+
+template <typename T>
+[[nodiscard]]
+std::size_t Vector<T>::capacity() const {
+    return data_.capacity();
+}
+
+template <typename T>
+void Vector<T>::resize(std::size_t new_size) {
+    data_.resize(new_size);
+}
 
 template <typename T>
 [[nodiscard]]
@@ -111,7 +139,7 @@ std::size_t Vector<T>::size() const {
 
 template <typename T>
 [[nodiscard]]
-T& Vector<T>::operator[](std::size_t i) {
+T& Vector<T>::operator[](std::size_t i) const {
     return data_[i];
 }
 
@@ -159,7 +187,7 @@ bool Vector<T>::operator!=(const Vector<T>& other) const {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator+(const T& scalar) {
+Vector<T> Vector<T>::operator+(const T& scalar) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] + scalar;
@@ -176,7 +204,7 @@ void Vector<T>::operator+=(const T& scalar) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator+(const Vector<T>& other) {
+Vector<T> Vector<T>::operator+(const Vector<T>& other) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] + other[i];
@@ -193,7 +221,7 @@ void Vector<T>::operator+=(const Vector<T>& other) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator-() {
+Vector<T> Vector<T>::operator-() const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = -(*this)[i];
@@ -203,7 +231,7 @@ Vector<T> Vector<T>::operator-() {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator-(const T& scalar) {
+Vector<T> Vector<T>::operator-(const T& scalar) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] - scalar;
@@ -220,7 +248,7 @@ void Vector<T>::operator-=(const T& scalar) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator-(const Vector<T>& other) {
+Vector<T> Vector<T>::operator-(const Vector<T>& other) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] - other[i];
@@ -237,7 +265,7 @@ void Vector<T>::operator-=(const Vector<T>& other) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator*(const T& scalar) {
+Vector<T> Vector<T>::operator*(const T& scalar) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] * scalar;
@@ -254,7 +282,7 @@ void Vector<T>::operator*=(const T& scalar) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator*(const Vector<T>& other) {
+Vector<T> Vector<T>::operator*(const Vector<T>& other) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] * other[i];
@@ -271,7 +299,7 @@ void Vector<T>::operator*=(const Vector<T>& other) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator/(const T& scalar) {
+Vector<T> Vector<T>::operator/(const T& scalar) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] / scalar;
@@ -288,7 +316,7 @@ void Vector<T>::operator/=(const T& scalar) {
 
 template <typename T>
 [[nodiscard]]
-Vector<T> Vector<T>::operator/(const Vector<T>& other) {
+Vector<T> Vector<T>::operator/(const Vector<T>& other) const {
     Vector<T> result(size());
     for (std::size_t i = 0; i < size(); ++i) {
         result[i] = (*this)[i] / other[i];
